c9adc3feafeb897c7fca5cdbc77ae179b7b4981f
Tancredi D'Anna <tancredi.danna@external.mobilesoft.it>
2022-11-04 10:27:03 +0100

    utils

--- app/src/main/java/com/example/marvel/util/Constants.kt
+++ app/src/main/java/com/example/marvel/util/Constants.kt
@@ -1,4 +1,29 @@
 package com.example.marvel.util
 
-object Constants {
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import java.sql.Timestamp
+
+class Constants {
+companion object{
+
+    fun getGradientVertical():Brush{
+        return Brush.verticalGradient(
+            colors = listOf(
+                Color(0xff4D090B),
+                Color(0xff0E0202),
+                Color(0xff000000)
+            )
+        )
+    }
+    fun getGradientHorizontal():Brush{
+        return Brush.horizontalGradient(
+            colors = listOf(
+                Color(0xff4D090B),
+                Color(0xff0E0202),
+                Color(0xff000000)
+            )
+        )
+    }
+}
 }
--- app/src/main/java/com/example/marvel/util/NetworkUtils.kt
+++ app/src/main/java/com/example/marvel/util/NetworkUtils.kt
@@ -1,26 +1,37 @@
 package com.example.marvel.util
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import com.example.marvel.domain.model.Character
 
-object MatchUtil {
+object NetworkUtils {
 
-    fun findMatch(singleCharacter: Character, searchedText: String):Boolean{
-        val singleStringDescription = singleCharacter.description.split(" ")
-
-        var foundMatchDescription:Boolean = false
-        for (i in singleStringDescription){
-            if (i.contains(searchedText, true)){
-                foundMatchDescription = true
+     fun isInternetAvailable(context: Context): Boolean {
+        var result = false
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val networkCapabilities = connectivityManager.activeNetwork ?: return false
+            val actNw = connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
+            result = when {
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                else -> false
+            }
+        } else {
+            connectivityManager.run {
+                connectivityManager.activeNetworkInfo?.run {
+                    result = when (type) {
+                        ConnectivityManager.TYPE_WIFI -> true
+                        ConnectivityManager.TYPE_MOBILE -> true
+                        ConnectivityManager.TYPE_ETHERNET -> true
+                        else -> false
+                    }
+                }
             }
         }
-
-        val singleStringName = singleCharacter.name.split(" ")
-        var foundMatchName:Boolean = false
-        for (i in singleStringName){
-            if (i.contains(searchedText, true)){
-                foundMatchName = true
-            }
-        }
-        return foundMatchDescription || foundMatchName
+        return result
     }
 }
