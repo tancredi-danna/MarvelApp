0b8ae85cff0a95f4d1b3109ecab61bf0bc9c89b8
Tancredi D'Anna <tancredi.danna@external.mobilesoft.it>
2022-11-04 10:26:02 +0100

    resources

--- app/src/main/java/com/example/marvel/util/Resource.kt
+++ app/src/main/java/com/example/marvel/util/Resource.kt
@@ -3,6 +3,6 @@ package com.example.marvel.util
 sealed class Resource<T>(val data: T? = null, val message: String? = null){
     class Success<T>(data: T?):Resource<T>(data)
     class Error<T>(message: String, data: T?):Resource<T>(data, message)
-    class Loading<T>(val isLoading: Boolean):Resource<T>(null)
+    class Loading<T>(val isLoading: Boolean = true):Resource<T>(null)
 }
 
